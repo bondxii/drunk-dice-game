ea23fa2ebfc9fa7f92b2592b52951dc409ca8f91
bondxii <ud.flock@gmail.com>
2024-04-27 11:19:21 +0300

    Big GUI update

--- game/logic.py
+++ game/logic.py
@@ -1,62 +1,15 @@
 # Here lies main logic of the game
 # Maybe it shuld be deleted after changing structure for GUI to work
 import random as r
+# import game.logic as ui
 
-class Single_player():
-    
-    user_score = 0
-    
-    # Main random method that generates nums 1-6    
-    def rolling(self) -> int:
+
+# Main gaming scripts
+def rolling() -> int:
         dice = r.randint(1, 6)
         return dice
     
-    # User decision to play or not
-    def ask_to_play(self) -> bool:
-        tries = 3
-        while tries > 0:
-            res = self.rolling()
-            print(res)
-            if res == 1:
-                return True
-            else:
-                print(f'You have {tries} tries.')
-                print('Want to make new roll?')
-                user_choice = input('[y/n]: ').lower()
-                if user_choice == 'y':
-                    tries -= 1
-                elif user_choice == 'n':
-                    self.user_score += res
-                    print(f'Your score is {self.user_score}')
-                    break
-                else:
-                    print('Wrong input!')
-        return False
-    
-    
-    
-    
-    def roll_check(self) -> None:
-        game = True
-        while game:
-            result = self.ask_to_play()
-            if result:
-                print('Game over! Bye')
-                game = False
-            else:
-                ai_score = r.randint(2, 6)
-                if self.user_score > ai_score:
-                    print(f'Ai: {ai_score} | You: {self.user_score}')
-                    print('You won!')
-                    game = False
-                elif self.user_score < ai_score:
-                    print(f'Ai: {ai_score} | You: {self.user_score}')
-                    print('You lose...')
-                    game = False
-                else:
-                    print(f'Ai: {ai_score} | You: {self.user_score}')
-                    print('It\'s a match!')
-                    game = False            
-        
-            
-        
+def ai_rolling() -> int:
+        dice = r.randint(2, 6)
+        return dice    
+                 
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # Main window to start the game logic
 
-from game.logic import Single_player
-import game.drunkui as gui
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QCoreApplication, Qt
+import gui.drunkui as gui
 
-def main():
-    s_p = Single_player()
-    s_p.roll_check()
-    # gui.root.mainloop()
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = gui.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
